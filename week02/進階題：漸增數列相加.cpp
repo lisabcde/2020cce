b6ae26385828c80006541e289fdbff760bd80d13
lisabcde <79676977+lisabcde@users.noreply.github.com>
2021-05-07 11:06:13 +0800

    Update 進階題：漸增數列相加.cpp

--- week02/進階題：漸增數列相加.cpp
+++ week02/進階題：漸增數列相加.cpp
@@ -10,3 +10,15 @@ int main()
 	}
 	printf("%d\n",ans);
 }
+
+or
+
+#include <stdio.h>
+int main()
+{
+	int n,ans=0;
+	scanf("%d,",&n);
+	for(int i=1;i<=n-1;i++)
+		ans+=i*(i+1);
+	printf("%d\n",ans);
+}
